8d970eb8893da389b675f1976e16bb65105aa33e
beomseok.choi <cbs412@naver.com>
2022-03-06 22:07:22 +0900

    Query with apolllo

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,24 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: +id },
+  });
+  console.log(data);
+  return <h1> {loading ? "loading" : data.movie.title}</h1>;
 }
 
 export default Detail;
